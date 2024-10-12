a3ed29d88f1cba70f43c8fb095c8305d74705ed8
st128198 <st128198@student.spbu.ru>
2024-10-13 01:44:33 +0300

    Update and rename Source1 to Source.cpp

--- 2024.09.28-Homework-2/Task01/Source.cpp
+++ 2024.09.28-Homework-2/Task01/Source.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 #include <cmath>
 
-int main(int argc, char* argv[]) {
-    int x1, y1, x2, y2;
+int main(int argc, char* argv[]) 
+
+{
+    
+    int x1=0, y1=0, x2=0, y2=0;
     scanf("%d %d %d %d", &x1, &y1, &x2, &y2);
     
     if (x1 == x2 || y1 == y2 || abs(x1 - x2) == abs(y1 - y2))
@@ -10,5 +13,6 @@ int main(int argc, char* argv[]) {
     else
         printf("NO");
 
-    return 0;
+    return EXIT_SUCCESS;
+    
 }
