b3fdaa4ba057313131328d7bde436340c23a85c2
st128198 <st128198@student.spbu.ru>
2024-10-13 01:38:29 +0300

    Update Source.cpp

--- 2024.09.18-Homework-1/Task04/Source.cpp
+++ 2024.09.18-Homework-1/Task04/Source.cpp
@@ -1,18 +1,20 @@
-#include<iostream>
+#include <iostream>
 
 int main(int argc, char* argv[])
+
 {
-int a=0;
-int b=0;
-int n=0;
+  
+    int a=0;
+    int b=0;
+    int n=0;
 
-scanf_s("%d", &a);
-scanf_s("%d", &b);
-scanf_s("%d", &n);
+    scanf_s("%d", &a);
+    scanf_s("%d", &b);
+    scanf_s("%d", &n);
 
-int x=2*a*b*n;
-printf("%d", x);
+    int x=2*a*b*n;
+    printf("%d", x);
 
-return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 
 }
