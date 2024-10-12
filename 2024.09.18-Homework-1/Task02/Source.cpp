396e19f5f1d425d9b33fa7996ca23904c3e5f412
st128198 <st128198@student.spbu.ru>
2024-10-13 01:36:50 +0300

    Update Source.cpp

--- 2024.09.18-Homework-1/Task02/Source.cpp
+++ 2024.09.18-Homework-1/Task02/Source.cpp
@@ -1,11 +1,13 @@
-#include<iostream>
+#include <iostream>
 
 int main(int argc, char* argv[])
-{
-int x=0;
-scanf_s("%d", &x);
-printf("%d", x);
 
-return EXIT_SUCCESS;
+{
+
+    int x=0;
+    scanf_s("%d", &x);
+    printf("%d", x);
+
+    return EXIT_SUCCESS;
 
 }
