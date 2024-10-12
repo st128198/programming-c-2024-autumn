efed3b2d9fbce1c328dab61079197f48338c2ffc
st128198 <st128198@student.spbu.ru>
2024-10-13 01:42:31 +0300

    Update Source.cpp

--- 2024.09.18-Homework-1/Task10/Source.cpp
+++ 2024.09.18-Homework-1/Task10/Source.cpp
@@ -1,7 +1,9 @@
-#include<iostream>
+#include <iostream>
 
 int main(int argc, char* argv[])
+
 {
+        
         int s=0;
         scanf_s("%d", &s);
         printf("%d %d %d", s/6, 4*s/6, s/6);
