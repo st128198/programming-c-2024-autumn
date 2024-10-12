fab7276dec1b375bf14f89edceaf56d2d5cc40fb
st128198 <st128198@student.spbu.ru>
2024-10-13 01:39:06 +0300

    Update Source.cpp

--- 2024.09.18-Homework-1/Task05/Source.cpp
+++ 2024.09.18-Homework-1/Task05/Source.cpp
@@ -1,7 +1,9 @@
-#include<iostream>
+#include <iostream>
 
 int main(int argc, char* argv[])
+
 {
+        
         int x=0;
         scanf_s("%d", &x);
         printf("The next number for the number ");
