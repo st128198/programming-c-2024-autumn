7a6cd5d965de893aada28764748efccc98a30d28
st128198 <st128198@student.spbu.ru>
2024-10-13 02:54:14 +0300

    Update Source.cpp

--- 2024.09.28-Homework-2/Task08/Source.cpp
+++ 2024.09.28-Homework-2/Task08/Source.cpp
@@ -1,1 +1,37 @@
+#include <cstdio>
 
+int main()
+
+{
+	
+    int n = 0;
+	int m = 0;
+
+	scanf("%d", &n);
+	scanf("%d", &m);
+
+	if (n < m)
+	{
+	int x = m;
+	m = n;
+	n = x;
+	}
+
+	if (n == 1 || m == 1)
+	{
+	printf("%d\n", n * m * 4);
+	}
+	else
+	{
+	int r = (m + 2) * n + m * (n + 2);
+	
+    if (n % 2 == 1 && m % 2 == 1)
+	{
+	r -= 2;
+	}
+	printf("%d\n", r);
+	}
+
+	return EXIT_SUCCESS;
+    
+}
