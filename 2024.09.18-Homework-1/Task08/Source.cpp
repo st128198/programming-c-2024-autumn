1b8c540d6f9776aa44010e5d4c16862c85a704d3
st128198 <st128198@student.spbu.ru>
2024-10-13 01:41:29 +0300

    Update Source.cpp

--- 2024.09.18-Homework-1/Task08/Source.cpp
+++ 2024.09.18-Homework-1/Task08/Source.cpp
@@ -1,7 +1,9 @@
-#include<iostream>
+#include <iostream>
 
 int main(int argc, char* argv[])
+
 {
+        
         int x=0;
         scanf_s("%d", &x);
         printf("%d", x/100+(x%100)/10+x%10);
