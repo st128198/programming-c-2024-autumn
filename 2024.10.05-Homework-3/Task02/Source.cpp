a066a71111ff8a2383d8a8d0a3713c19d3caa9e1
st128198 <st128198@student.spbu.ru>
2024-10-13 04:13:45 +0300

    Update Source.cpp

--- 2024.10.05-Homework-3/Task02/Source.cpp
+++ 2024.10.05-Homework-3/Task02/Source.cpp
@@ -1,1 +1,29 @@
+#include <stdio.h>
+#include <iomanip>
 
+int main(int argc, char* argv[]) 
+
+{
+    
+    int k=0;
+    scanf("%d", &k);
+    
+    for (int i = 0; i < k; i++) 
+    {
+        int n=0, m=0;
+        scanf("%d %d", &n, &m);
+        double d = 19 * m + (n + 239) * (n + 366) / 2.0;
+        
+        if (d == (int)d) 
+        {
+            printf("%d\n", (int)d);
+        } 
+        else 
+        {
+            printf("%.6f\n", d);
+        }
+    }
+    
+    return EXIT_SUCCESS;
+    
+}
