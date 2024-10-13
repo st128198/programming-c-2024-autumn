1114b2b1cb11b2a6e5b20fc95f81c4550f169032
st128198 <st128198@student.spbu.ru>
2024-10-13 04:18:08 +0300

    Update Source.cpp

--- 2024.10.05-Homework-3/Task03/Source.cpp
+++ 2024.10.05-Homework-3/Task03/Source.cpp
@@ -1,1 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main() 
+
+{
+  
+    int n = 0;
+    scanf("%d", &n);
+    int b[n];
+    
+    int c = 0;
+
+    for (int i = 0; i < n; i++) 
+    {
+        scanf("%d", &b[i]);
+    }
+
+    for (int i = 0; i < n; i++) 
+    {
+        if (b[i] <= 437) 
+        {
+            printf("Crash %d\n", i + 1);
+            c = 1;
+            break;
+        }
+    }
+
+    if (c == 0) 
+    {
+        printf("No crash\n");
+    }
+
+    return EXIT_SUCCESS;
+  
+}
