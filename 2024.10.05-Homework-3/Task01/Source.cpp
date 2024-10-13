5998062a4eb731028fa0cea16d0e2b5850c5f332
st128198 <st128198@student.spbu.ru>
2024-10-13 04:11:48 +0300

    Update Source.cpp

--- 2024.10.05-Homework-3/Task01/Source.cpp
+++ 2024.10.05-Homework-3/Task01/Source.cpp
@@ -1,1 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main()
+
+{
+    
+    long n=0, x=0, S0=0, S1=0, i=0;
+    scanf("%ld", &n);
+    
+    S0 = S1 = 0;
+    for (i = 1; i <= n; i++) 
+    {
+        scanf("%ld", &x);
+        x > 0 ? S0++ : S1++;
+    }
+
+    printf("%ld\n", S0 < S1 ? S0 : S1);
+
+    return EXIT_SUCCESS;
+    
+}
