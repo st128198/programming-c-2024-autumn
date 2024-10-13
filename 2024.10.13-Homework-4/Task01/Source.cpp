6522888cb4b6693208c26e2abda33b3d6ce720bf
st128198 <st128198@student.spbu.ru>
2024-10-13 05:36:52 +0300

    Update Source.cpp

--- 2024.10.13-Homework-4/Task01/Source.cpp
+++ 2024.10.13-Homework-4/Task01/Source.cpp
@@ -1,1 +1,23 @@
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
 
+int main() 
+
+{
+    
+    int n=0;
+    scanf("%d", &n);
+
+    vector<int> a(n);
+    for (int i = 0; i < n; ++i)
+        scanf("%d", &a[i]);
+
+    int x=0;
+    scanf("%d", &x);
+
+    printf("%d\n", count(a.cbegin(), a.cend(), x));
+
+    return EXIT_SUCCESS;
+    
+}
