d35b8ed3d62917cd9952b22c16f18fdb51c3a51a
st128198 <st128198@student.spbu.ru>
2024-10-13 06:46:07 +0300

    Update Source.cpp

--- 2024.10.13-Homework-4/Task10/Source.cpp
+++ 2024.10.13-Homework-4/Task10/Source.cpp
@@ -1,1 +1,39 @@
+#include <stdio.h>
 
+int main() 
+
+{
+    
+    int n=0, a=0, b=0, c=0, d=0;
+    scanf("%d %d %d %d %d", &n, &a, &b, &c, &d);
+
+    int x[1001], y[1001];
+    for(int i = 1; i <= n; i++) 
+    {
+        x[i] = i; 
+        y[i] = x[i];
+    }
+    
+    for(int i = 0; i <= b - a; i++) 
+    {
+        x[a + i] = y[b - i];
+    }
+    
+    for(int i = 1; i <= n; i++) 
+    {
+        y[i] = x[i];
+    }
+ 
+    for(int i = 0; i <= d - c; i++) 
+    {
+        x[c + i] = y[d - i];
+    }
+ 
+    for(int i = 1; i <= n; i++) 
+    {
+        printf("%d ", x[i]);
+    }
+    
+    return EXIT_SUCCESS;
+  
+}
