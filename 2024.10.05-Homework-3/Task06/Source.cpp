c2154b003bbe7bc645f1fd5476b498908c381235
st128198 <st128198@student.spbu.ru>
2024-10-13 04:43:48 +0300

    Update Source.cpp

--- 2024.10.05-Homework-3/Task06/Source.cpp
+++ 2024.10.05-Homework-3/Task06/Source.cpp
@@ -1,1 +1,31 @@
+#include <stdio.h>
 
+int nod(int a, int b) 
+{
+    if (b == 0) 
+    {
+        return a;
+    } 
+    else 
+    {
+        return nod(b, a % b);
+    }
+}
+
+int nok(int a, int b) 
+{
+    return a * b / nod(a, b);
+}
+
+int main()
+
+{
+    
+    int a, b;
+    scanf("%d", &a);
+    scanf("%d", &b);
+    printf("%d\n", nok(a, b));
+   
+    return 0;
+    
+}
