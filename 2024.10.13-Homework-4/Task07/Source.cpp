09e2a9cc9f815876c193d2601eebf10756a8ccf9
st128198 <st128198@student.spbu.ru>
2024-10-13 06:23:14 +0300

    Update Source.cpp

--- 2024.10.13-Homework-4/Task07/Source.cpp
+++ 2024.10.13-Homework-4/Task07/Source.cpp
@@ -1,1 +1,23 @@
+#include <stdio.h>
 
+int main() 
+
+{
+    
+    int n=0;
+    scanf("%d", &n);
+    
+    int a[n];
+    for(int i = 0; i < n; ++i) 
+    {
+        scanf("%d", &a[i]);
+    }
+
+    for(int i = n - 1; i >= 0; --i) 
+    {
+        printf("%d ", a[i]);
+    }
+    
+    return EXIT_SUCCESS;
+    
+}
