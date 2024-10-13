e45251122731b45b124ec8862b214d6b3096c86f
st128198 <st128198@student.spbu.ru>
2024-10-13 05:18:41 +0300

    Update Source.cpp

--- 2024.10.05-Homework-3/Task10/Source.cpp
+++ 2024.10.05-Homework-3/Task10/Source.cpp
@@ -1,1 +1,31 @@
+#include <stdio.h>
 
+int main() 
+
+{
+   
+    int n=0, k=0, ans=0, a[101];
+    scanf("%d", &n);
+    
+    for (int i = 0; i < n; i++)
+        scanf("%d", &a[i]);
+
+    k = ans = 0;
+    for (int i = 0; i < n; i++)
+        if (a[i] > 0)
+            k++;
+        else 
+        {
+            if (k > ans)
+                ans = k;
+            k = 0;
+        }
+
+    if (k > ans)
+        ans = k;
+
+    printf("%d\n", ans);
+
+    return EXIT_SUCCESS;
+    
+}
