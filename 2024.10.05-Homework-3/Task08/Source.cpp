33895d4d5a35a688fb56a81d8fe2705875e601ea
st128198 <st128198@student.spbu.ru>
2024-10-13 05:08:22 +0300

    Update Source.cpp

--- 2024.10.05-Homework-3/Task08/Source.cpp
+++ 2024.10.05-Homework-3/Task08/Source.cpp
@@ -1,1 +1,33 @@
+#include <stdio.h>
 
+int gcd(int m, int n) 
+
+{
+    
+  while(m && n) {
+        if (m < n) 
+            n %= m; 
+        else 
+            m %= n;
+    }
+    return m + n;
+}
+
+int main() 
+
+{
+    int i, j, f0 = 0, f1 = 1;
+    scanf("%d %d", &i, &j);
+
+    for(i = gcd(i, j); i > 0; --i) 
+    {
+        int f = (f0 + f1) % 1000000000;
+        f0 = f1;
+        f1 = f;
+    }
+
+    printf("%d\n", f0);
+  
+    return EXIT_SUCCESS;
+  
+}
