9db408c9e10e73d6ba935b593ee64f8675840cea
st128198 <st128198@student.spbu.ru>
2024-10-13 04:28:19 +0300

    Update Source.cpp

--- 2024.10.05-Homework-3/Task05/Source.cpp
+++ 2024.10.05-Homework-3/Task05/Source.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+#include <stdio.h>
+int main()
 
+{
+    
+  int n=0, v=0, c=0;
+    scanf("%d", &n);
+    for(int i=0; i<n*n; i++)
+    {
+        scanf("%d", &v);
+        if(v==1)
+        {
+            c++;
+        }
+    }
+    printf("%d", c/2);
+
+return EXIT_SUCCESS;
+
+}
