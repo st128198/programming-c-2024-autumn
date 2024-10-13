3b8ce9e2402886f8710c16620eee8603e925f4e7
st128198 <st128198@student.spbu.ru>
2024-10-13 05:54:21 +0300

    Update Source.cpp

--- 2024.10.13-Homework-4/Task03/Source.cpp
+++ 2024.10.13-Homework-4/Task03/Source.cpp
@@ -1,1 +1,31 @@
+#include <stdio.h>
 
+int main() 
+
+{
+    
+    int a[1000], i=0, n=0, x=0, y=0;
+
+    scanf("%d %d", &n, &a[0]);
+    x = a[0];
+    y = a[0];
+
+    for (i = 1; i < n; i++) 
+    {
+        scanf("%d", &a[i]);
+        if (a[i] > x) 
+            x = a[i];
+        else if (a[i] < y) 
+            y = a[i];
+    }
+
+    for (i = 0; i < n; i++) 
+    {
+        if (a[i] == x) 
+            a[i] = y;
+        printf("%d ", a[i]);
+    }
+
+    return EXIT_SUCCESS;
+    
+}
