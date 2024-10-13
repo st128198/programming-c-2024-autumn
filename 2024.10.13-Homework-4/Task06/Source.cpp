cef71be6150a74465f79a3e04e9848a5e9acc95a
st128198 <st128198@student.spbu.ru>
2024-10-13 06:18:38 +0300

    Update Source.cpp

--- 2024.10.13-Homework-4/Task06/Source.cpp
+++ 2024.10.13-Homework-4/Task06/Source.cpp
@@ -1,1 +1,28 @@
+#include <stdio.h>
 
+int v[100], n=0, m=-1, i=0, j=0, d=0;
+
+#define x
+
+int main() 
+
+{
+    
+    x("%d", &n);
+
+    for (i = 0; i < n; x("%d", &v[i++]));
+
+    for (j = 0; j < n;) 
+    {
+        if ((n *= v[j++]) > m) 
+        {
+            m = n;
+            d = j;
+        }
+    }
+    
+    printf("%d\n", d);
+    
+    return EXIT_SUCCESS;
+    
+}
