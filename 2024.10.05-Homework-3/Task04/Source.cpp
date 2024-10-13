b4ea5e745a9692bafda1b6a80085eaa0087b8f8b
st128198 <st128198@student.spbu.ru>
2024-10-13 04:23:10 +0300

    Update Source.cpp

--- 2024.10.05-Homework-3/Task04/Source.cpp
+++ 2024.10.05-Homework-3/Task04/Source.cpp
@@ -1,1 +1,29 @@
+#include <stdio.h>
 
+int main() 
+
+{
+    
+    int n=0;
+    scanf("%d", &n);
+    
+    int v = 0, s = 0, vx = -1, a = 0, vm = 0;
+    for (int i = 0; i < n; i++) 
+    {
+        a += 1;
+        scanf("%d %d", &v, &s);
+        if (s == 1) 
+        {
+            if (vm < v) 
+            {
+                vx = a;
+                vm = v;
+            }
+        }
+    }
+    
+    printf("%d\n", vx);
+    
+    return EXIT_SUCCESS;
+    
+}
